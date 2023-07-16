e2d83ae4a0dfa908e2199c094140c415a05a92cd
tungtdph16451@fpt.edu.vn <tungtdph16451@fpt.edu.vn>
2023-07-16 22:20:59 +0700

    calcultor area

--- PhanLoaiTraiCam/Views/_Home.Designer.cs
+++ PhanLoaiTraiCam/Views/_Home.Designer.cs
@@ -31,22 +31,23 @@
             components = new System.ComponentModel.Container();
             tbl_Main = new TableLayoutPanel();
             tbl_Menu = new TableLayoutPanel();
-            label2 = new Label();
-            label1 = new Label();
             lbl_Device = new Label();
             cmb_Device = new ComboBox();
-            cmb_CNN = new ComboBox();
-            cmb_Count = new ComboBox();
             btn_Start = new Button();
             btn_Stop = new Button();
+            btn_TakePhoto = new Button();
             tbl_Container = new TableLayoutPanel();
+            pic_GrayScale = new PictureBox();
             pic_Video2 = new PictureBox();
             pic_Video1 = new PictureBox();
             dtgrid_Result = new DataGridView();
+            Column1 = new DataGridViewTextBoxColumn();
+            Column2 = new DataGridViewTextBoxColumn();
             timer_video = new System.Windows.Forms.Timer(components);
             tbl_Main.SuspendLayout();
             tbl_Menu.SuspendLayout();
             tbl_Container.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)pic_GrayScale).BeginInit();
             ((System.ComponentModel.ISupportInitialize)pic_Video2).BeginInit();
             ((System.ComponentModel.ISupportInitialize)pic_Video1).BeginInit();
             ((System.ComponentModel.ISupportInitialize)dtgrid_Result).BeginInit();
@@ -60,69 +61,46 @@
             tbl_Main.Controls.Add(tbl_Container, 0, 1);
             tbl_Main.Dock = DockStyle.Fill;
             tbl_Main.Location = new Point(0, 0);
+            tbl_Main.Margin = new Padding(3, 4, 3, 4);
             tbl_Main.Name = "tbl_Main";
             tbl_Main.RowCount = 2;
             tbl_Main.RowStyles.Add(new RowStyle(SizeType.Percent, 5.9558115F));
             tbl_Main.RowStyles.Add(new RowStyle(SizeType.Percent, 94.04419F));
-            tbl_Main.Size = new Size(1904, 1041);
+            tbl_Main.Size = new Size(1924, 1055);
             tbl_Main.TabIndex = 0;
             // 
             // tbl_Menu
             // 
             tbl_Menu.ColumnCount = 6;
-            tbl_Menu.ColumnStyles.Add(new ColumnStyle(SizeType.Absolute, 165F));
-            tbl_Menu.ColumnStyles.Add(new ColumnStyle(SizeType.Absolute, 150F));
-            tbl_Menu.ColumnStyles.Add(new ColumnStyle(SizeType.Absolute, 114F));
-            tbl_Menu.ColumnStyles.Add(new ColumnStyle(SizeType.Absolute, 138F));
-            tbl_Menu.ColumnStyles.Add(new ColumnStyle(SizeType.Absolute, 127F));
+            tbl_Menu.ColumnStyles.Add(new ColumnStyle(SizeType.Absolute, 189F));
+            tbl_Menu.ColumnStyles.Add(new ColumnStyle(SizeType.Absolute, 158F));
+            tbl_Menu.ColumnStyles.Add(new ColumnStyle(SizeType.Absolute, 143F));
+            tbl_Menu.ColumnStyles.Add(new ColumnStyle(SizeType.Absolute, 158F));
+            tbl_Menu.ColumnStyles.Add(new ColumnStyle(SizeType.Absolute, 145F));
             tbl_Menu.ColumnStyles.Add(new ColumnStyle(SizeType.Percent, 100F));
-            tbl_Menu.Controls.Add(label2, 2, 0);
-            tbl_Menu.Controls.Add(label1, 1, 0);
             tbl_Menu.Controls.Add(lbl_Device, 0, 0);
             tbl_Menu.Controls.Add(cmb_Device, 0, 1);
-            tbl_Menu.Controls.Add(cmb_CNN, 1, 1);
-            tbl_Menu.Controls.Add(cmb_Count, 2, 1);
-            tbl_Menu.Controls.Add(btn_Start, 3, 1);
-            tbl_Menu.Controls.Add(btn_Stop, 4, 1);
+            tbl_Menu.Controls.Add(btn_Start, 1, 1);
+            tbl_Menu.Controls.Add(btn_Stop, 2, 1);
+            tbl_Menu.Controls.Add(btn_TakePhoto, 3, 1);
             tbl_Menu.Dock = DockStyle.Fill;
-            tbl_Menu.Location = new Point(3, 3);
+            tbl_Menu.Location = new Point(3, 4);
+            tbl_Menu.Margin = new Padding(3, 4, 3, 4);
             tbl_Menu.Name = "tbl_Menu";
             tbl_Menu.RowCount = 2;
-            tbl_Menu.RowStyles.Add(new RowStyle(SizeType.Percent, 39.2857132F));
-            tbl_Menu.RowStyles.Add(new RowStyle(SizeType.Percent, 60.7142868F));
-            tbl_Menu.Size = new Size(1898, 56);
+            tbl_Menu.RowStyles.Add(new RowStyle(SizeType.Percent, 33.3333321F));
+            tbl_Menu.RowStyles.Add(new RowStyle(SizeType.Percent, 66.6666641F));
+            tbl_Menu.Size = new Size(1918, 54);
             tbl_Menu.TabIndex = 0;
-            // 
-            // label2
-            // 
-            label2.AccessibleDescription = "Device";
-            label2.AutoSize = true;
-            label2.Dock = DockStyle.Bottom;
-            label2.Location = new Point(318, 7);
-            label2.Name = "label2";
-            label2.Size = new Size(108, 15);
-            label2.TabIndex = 2;
-            label2.Text = "Count";
-            // 
-            // label1
-            // 
-            label1.AccessibleDescription = "Device";
-            label1.AutoSize = true;
-            label1.Dock = DockStyle.Bottom;
-            label1.Location = new Point(168, 7);
-            label1.Name = "label1";
-            label1.Size = new Size(144, 15);
-            label1.TabIndex = 1;
-            label1.Text = "CNN";
             // 
             // lbl_Device
             // 
             lbl_Device.AccessibleDescription = "Device";
             lbl_Device.AutoSize = true;
             lbl_Device.Dock = DockStyle.Bottom;
-            lbl_Device.Location = new Point(3, 7);
+            lbl_Device.Location = new Point(3, 0);
             lbl_Device.Name = "lbl_Device";
-            lbl_Device.Size = new Size(159, 15);
+            lbl_Device.Size = new Size(183, 17);
             lbl_Device.TabIndex = 0;
             lbl_Device.Text = "Device";
             // 
@@ -131,39 +109,18 @@
             cmb_Device.Dock = DockStyle.Bottom;
             cmb_Device.DropDownStyle = ComboBoxStyle.DropDownList;
             cmb_Device.FormattingEnabled = true;
-            cmb_Device.Location = new Point(3, 30);
+            cmb_Device.Location = new Point(3, 22);
+            cmb_Device.Margin = new Padding(3, 4, 3, 4);
             cmb_Device.Name = "cmb_Device";
-            cmb_Device.Size = new Size(159, 23);
+            cmb_Device.Size = new Size(183, 28);
             cmb_Device.TabIndex = 3;
-            // 
-            // cmb_CNN
-            // 
-            cmb_CNN.Dock = DockStyle.Bottom;
-            cmb_CNN.DropDownStyle = ComboBoxStyle.DropDownList;
-            cmb_CNN.FormattingEnabled = true;
-            cmb_CNN.Location = new Point(168, 30);
-            cmb_CNN.Name = "cmb_CNN";
-            cmb_CNN.Size = new Size(144, 23);
-            cmb_CNN.TabIndex = 4;
-            // 
-            // cmb_Count
-            // 
-            cmb_Count.Dock = DockStyle.Bottom;
-            cmb_Count.DropDownStyle = ComboBoxStyle.DropDownList;
-            cmb_Count.FormattingEnabled = true;
-            cmb_Count.Items.AddRange(new object[] { "10", "20", "50", "100", "200", "500" });
-            cmb_Count.Location = new Point(318, 30);
-            cmb_Count.Name = "cmb_Count";
-            cmb_Count.Size = new Size(108, 23);
-            cmb_Count.TabIndex = 5;
             // 
             // btn_Start
             // 
-            btn_Start.Dock = DockStyle.Bottom;
-            btn_Start.Location = new Point(444, 30);
-            btn_Start.Margin = new Padding(15, 3, 15, 3);
+            btn_Start.Location = new Point(206, 21);
+            btn_Start.Margin = new Padding(17, 4, 17, 4);
             btn_Start.Name = "btn_Start";
-            btn_Start.Size = new Size(108, 23);
+            btn_Start.Size = new Size(124, 29);
             btn_Start.TabIndex = 6;
             btn_Start.Text = "Start";
             btn_Start.UseVisualStyleBackColor = true;
@@ -171,40 +128,65 @@
             // 
             // btn_Stop
             // 
-            btn_Stop.Dock = DockStyle.Bottom;
-            btn_Stop.Location = new Point(582, 30);
-            btn_Stop.Margin = new Padding(15, 3, 15, 3);
+            btn_Stop.Location = new Point(364, 21);
+            btn_Stop.Margin = new Padding(17, 4, 17, 4);
             btn_Stop.Name = "btn_Stop";
-            btn_Stop.Size = new Size(97, 23);
+            btn_Stop.Size = new Size(109, 29);
             btn_Stop.TabIndex = 7;
             btn_Stop.Text = "Stop";
             btn_Stop.UseVisualStyleBackColor = true;
             btn_Stop.Click += btn_Stop_Click;
+            // 
+            // btn_TakePhoto
+            // 
+            btn_TakePhoto.Location = new Point(507, 21);
+            btn_TakePhoto.Margin = new Padding(17, 4, 17, 4);
+            btn_TakePhoto.Name = "btn_TakePhoto";
+            btn_TakePhoto.Size = new Size(124, 29);
+            btn_TakePhoto.TabIndex = 8;
+            btn_TakePhoto.Text = "Chụp";
+            btn_TakePhoto.UseVisualStyleBackColor = true;
+            btn_TakePhoto.Click += btn_TakePhoto_Click;
             // 
             // tbl_Container
             // 
             tbl_Container.ColumnCount = 2;
             tbl_Container.ColumnStyles.Add(new ColumnStyle(SizeType.Percent, 50F));
             tbl_Container.ColumnStyles.Add(new ColumnStyle(SizeType.Percent, 50F));
+            tbl_Container.Controls.Add(pic_GrayScale, 0, 1);
             tbl_Container.Controls.Add(pic_Video2, 1, 0);
             tbl_Container.Controls.Add(pic_Video1, 0, 0);
             tbl_Container.Controls.Add(dtgrid_Result, 1, 1);
             tbl_Container.Dock = DockStyle.Fill;
-            tbl_Container.Location = new Point(3, 65);
+            tbl_Container.Location = new Point(3, 66);
+            tbl_Container.Margin = new Padding(3, 4, 3, 4);
             tbl_Container.Name = "tbl_Container";
             tbl_Container.RowCount = 2;
             tbl_Container.RowStyles.Add(new RowStyle(SizeType.Percent, 50F));
             tbl_Container.RowStyles.Add(new RowStyle(SizeType.Percent, 50F));
-            tbl_Container.Size = new Size(1898, 973);
+            tbl_Container.Size = new Size(1918, 985);
             tbl_Container.TabIndex = 1;
+            // 
+            // pic_GrayScale
+            // 
+            pic_GrayScale.BackColor = SystemColors.ActiveBorder;
+            pic_GrayScale.Dock = DockStyle.Fill;
+            pic_GrayScale.Location = new Point(3, 496);
+            pic_GrayScale.Margin = new Padding(3, 4, 3, 4);
+            pic_GrayScale.Name = "pic_GrayScale";
+            pic_GrayScale.Size = new Size(953, 485);
+            pic_GrayScale.SizeMode = PictureBoxSizeMode.StretchImage;
+            pic_GrayScale.TabIndex = 3;
+            pic_GrayScale.TabStop = false;
             // 
             // pic_Video2
             // 
             pic_Video2.BackColor = SystemColors.ActiveBorder;
             pic_Video2.Dock = DockStyle.Fill;
-            pic_Video2.Location = new Point(952, 3);
+            pic_Video2.Location = new Point(962, 4);
+            pic_Video2.Margin = new Padding(3, 4, 3, 4);
             pic_Video2.Name = "pic_Video2";
-            pic_Video2.Size = new Size(943, 480);
+            pic_Video2.Size = new Size(953, 484);
             pic_Video2.SizeMode = PictureBoxSizeMode.StretchImage;
             pic_Video2.TabIndex = 1;
             pic_Video2.TabStop = false;
@@ -213,23 +195,41 @@
             // 
             pic_Video1.BackColor = SystemColors.ActiveBorder;
             pic_Video1.Dock = DockStyle.Fill;
-            pic_Video1.Location = new Point(3, 3);
+            pic_Video1.Location = new Point(3, 4);
+            pic_Video1.Margin = new Padding(3, 4, 3, 4);
             pic_Video1.Name = "pic_Video1";
-            pic_Video1.Size = new Size(943, 480);
+            pic_Video1.Size = new Size(953, 484);
             pic_Video1.SizeMode = PictureBoxSizeMode.StretchImage;
             pic_Video1.TabIndex = 0;
             pic_Video1.TabStop = false;
             // 
             // dtgrid_Result
             // 
-            dtgrid_Result.BackgroundColor = SystemColors.ActiveBorder;
+            dtgrid_Result.BackgroundColor = Color.White;
             dtgrid_Result.ColumnHeadersHeightSizeMode = DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            dtgrid_Result.Columns.AddRange(new DataGridViewColumn[] { Column1, Column2 });
             dtgrid_Result.Dock = DockStyle.Fill;
-            dtgrid_Result.Location = new Point(952, 489);
+            dtgrid_Result.Location = new Point(962, 496);
+            dtgrid_Result.Margin = new Padding(3, 4, 3, 4);
             dtgrid_Result.Name = "dtgrid_Result";
+            dtgrid_Result.RowHeadersWidthSizeMode = DataGridViewRowHeadersWidthSizeMode.AutoSizeToDisplayedHeaders;
             dtgrid_Result.RowTemplate.Height = 25;
-            dtgrid_Result.Size = new Size(943, 481);
+            dtgrid_Result.Size = new Size(953, 485);
             dtgrid_Result.TabIndex = 2;
+            // 
+            // Column1
+            // 
+            Column1.HeaderText = "Column1";
+            Column1.MinimumWidth = 6;
+            Column1.Name = "Column1";
+            Column1.Width = 125;
+            // 
+            // Column2
+            // 
+            Column2.HeaderText = "Column2";
+            Column2.MinimumWidth = 6;
+            Column2.Name = "Column2";
+            Column2.Width = 125;
             // 
             // timer_video
             // 
@@ -237,10 +237,11 @@
             // 
             // _Home
             // 
-            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleDimensions = new SizeF(8F, 20F);
             AutoScaleMode = AutoScaleMode.Font;
-            ClientSize = new Size(1904, 1041);
+            ClientSize = new Size(1924, 1055);
             Controls.Add(tbl_Main);
+            Margin = new Padding(3, 4, 3, 4);
             Name = "_Home";
             StartPosition = FormStartPosition.CenterScreen;
             Text = "_Home";
@@ -250,6 +251,7 @@
             tbl_Menu.ResumeLayout(false);
             tbl_Menu.PerformLayout();
             tbl_Container.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)pic_GrayScale).EndInit();
             ((System.ComponentModel.ISupportInitialize)pic_Video2).EndInit();
             ((System.ComponentModel.ISupportInitialize)pic_Video1).EndInit();
             ((System.ComponentModel.ISupportInitialize)dtgrid_Result).EndInit();
@@ -260,12 +262,8 @@
 
         private TableLayoutPanel tbl_Main;
         private TableLayoutPanel tbl_Menu;
-        private Label label2;
-        private Label label1;
         private Label lbl_Device;
         private ComboBox cmb_Device;
-        private ComboBox cmb_CNN;
-        private ComboBox cmb_Count;
         private Button btn_Start;
         private Button btn_Stop;
         private TableLayoutPanel tbl_Container;
@@ -273,5 +271,9 @@
         private PictureBox pic_Video2;
         private DataGridView dtgrid_Result;
         private System.Windows.Forms.Timer timer_video;
+        private Button btn_TakePhoto;
+        private PictureBox pic_GrayScale;
+        private DataGridViewTextBoxColumn Column1;
+        private DataGridViewTextBoxColumn Column2;
     }
 }
--- PhanLoaiTraiCam/Views/_Home.cs
+++ PhanLoaiTraiCam/Views/_Home.cs
@@ -1,4 +1,6 @@
-﻿using AForge.Video;
+﻿using AForge.Imaging;
+using AForge.Imaging.Filters;
+using AForge.Video;
 using AForge.Video.DirectShow;
 using PhanLoaiTraiCam.Utilities.Extensions;
 using System;
@@ -10,6 +12,8 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using static System.Windows.Forms.VisualStyles.VisualStyleElement;
+using static System.Windows.Forms.VisualStyles.VisualStyleElement.Button;
 
 namespace PhanLoaiTraiCam.Views
 {
@@ -17,6 +21,8 @@ namespace PhanLoaiTraiCam.Views
     {
         private FilterInfoCollection _devices;
         private VideoCaptureDevice cam;
+        private Bitmap afterSuppression;
+        private Bitmap afterEdgeTrack;
         public _Home()
         {
             InitializeComponent();
@@ -45,6 +51,7 @@ namespace PhanLoaiTraiCam.Views
         }
         private void FinalFrame_NewFrame(object sender, NewFrameEventArgs eventArgs)
         {
+            if (pic_Video1.Image != null) { pic_Video1.Image.Dispose(); }
             pic_Video1.Image = (Bitmap)eventArgs.Frame.Clone();
         }
 
@@ -68,8 +75,38 @@ namespace PhanLoaiTraiCam.Views
         {
             if (pic_Video1.Image != null)
             {
-                pic_Video2.Image = ((Bitmap)pic_Video1.Image.Clone()).ToGrayScale();
             }
+        }
+
+        private void btn_TakePhoto_Click(object sender, EventArgs e)
+        {
+            var image = (Bitmap)pic_Video1.Image.Clone();
+            if (pic_Video2.Image != null) { pic_Video2.Image.Dispose(); }
+            pic_Video2.Image = image;
+            if (pic_GrayScale.Image != null) { pic_GrayScale.Image.Dispose(); }
+            Bitmap  afterGrey = image.ImageToGrey();
+            Bitmap afterGauss;
+            double sigma = 0;
+            afterGauss = afterGrey.GaussianFilter(sigma);
+            Bitmap afterSobel = afterGauss.SobelConvolve();
+            afterSuppression = afterSobel.NonMaximumSuppression();
+            Bitmap afterThreshold = afterSuppression.DoubleThreshold(100, 10);
+            afterEdgeTrack = afterThreshold.EdgeTracking();
+            Bitmap afterRestoration = afterEdgeTrack.BorderRestoration(3);
+            pic_GrayScale.Image = afterThreshold;
+            // Phân ngưỡng ảnh xám để tách vùng đối tượng
+            // Tạo bộ lọc liên kết để nối các điểm ảnh thành vùng
+            BlobCounter blobCounter = new BlobCounter();
+            blobCounter.ProcessImage(afterThreshold);
+            Blob[] blobs = blobCounter.GetObjectsInformation();
+
+            // Tính tổng diện tích của các vùng
+            int totalArea = 0;
+            foreach (Blob blob in blobs)
+            {
+                totalArea += blob.Area;
+            }
+            MessageBox.Show(totalArea.ToString());
         }
     }
 }
